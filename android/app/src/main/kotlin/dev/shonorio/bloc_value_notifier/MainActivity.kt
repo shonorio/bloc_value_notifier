5553e8307f0f94dcd9b2771edba9f1437ad72cf9
Solli Moreira Honorio <shonorio@gmail.com>
2023-09-13 18:01:40 +0200

    feat: initial project setup

---
+++ android/app/src/main/kotlin/dev/shonorio/bloc_value_notifier/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.shonorio.bloc_value_notifier
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presenter/my_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'page/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Value Notifier',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
